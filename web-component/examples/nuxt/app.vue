e34a340797bf1dbda94b152bdd84709a63ae3a99
Ayoub Adib <adbayb@gmail.com>
2022-05-11 19:20:56 +0200

    chore(web-component): add Nuxt example

---
+++ web-component/examples/nuxt/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Button :variation="variation" :onPress="onPress">Hello world</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { onMounted } from "vue"
+import Button from "./components/Button"
+
+export default {
+  components: { Button },
+  data() {
+    return {
+      variation: "secondary"
+    }
+  }, 
+  mounted() {
+    setInterval(() => {
+      this.variation = this.variation === "primary" ? "secondary" : "primary"
+    }, 1000)
+  },
+  methods: {
+    onPress: () => {
+      console.log("click")
+    }
+  }
+}
+</script>
---
+++ web-component/examples/nuxt/components/Button.tsx
@@ -0,0 +1,31 @@
+import { defineComponent, h } from "vue";
+import { Button, createCustomElement } from "@web-component/components";
+
+// @todo: transform it to generic adapter
+// { components: { VueButton: createVueComponent(Button) } }
+export default defineComponent({
+	name: "VueButton", // optional
+	props: ["variation", "onPress"], // @todo: get props dynamically
+	setup(props, ctx) {
+		const mapPropsToOutsideContract = () => {
+			return {
+				...props,
+				children: ctx.slots.default()[0].children as string,
+			};
+		};
+		const mappedProps = reactive({
+			current: mapPropsToOutsideContract(),
+		});
+		const content = createCustomElement("wc-button", Button, mappedProps);
+
+		onBeforeUpdate(() => {
+			mappedProps.current = mapPropsToOutsideContract();
+		});
+
+		return () =>
+			h("wc-button", {
+				...props,
+				innerHTML: typeof window === "undefined" ? content : undefined,
+			});
+	},
+});
